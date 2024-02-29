7c863e09afbfffdbcb242be7ccb652aae9e44285
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-02-29 17:13:30 +0100

    Add v1 dynamic textures

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import vertex from "./shaders/vertex.glsl";
 import fragment from "./shaders/fragment.glsl";
 import { Uniform } from "three";
@@ -7,28 +7,38 @@ import { useControls } from "leva";
 import gsap from "gsap";
 
 export const Image = () => {
+  const [currentWall, setCurrentWall] = useState("/brick.webp");
+  const [nextWall, setNextWall] = useState("/brick.webp");
+
   const texture1 = useTexture("/env.webp");
-  const texture2 = useTexture("/brick.webp");
-  const texture3 = useTexture("/wood.webp");
 
   const uniforms = useMemo(
     () => ({
       uEnvironment: new Uniform(texture1),
-      uImageBrick1: new Uniform(texture2),
-      uImageBrick2: new Uniform(texture3),
+      uImageBrick1: new Uniform(),
+      uImageBrick2: new Uniform(),
       uProgress: new Uniform(0),
     }),
     []
   );
 
+  useTexture([currentWall, nextWall], (textures) => {
+    uniforms.uImageBrick1.value = textures[0];
+    uniforms.uImageBrick2.value = textures[1];
+  });
+
   const test = useControls({
     wallType: {
       options: {
-        brick: 0,
-        wood: 1,
+        brick: "/brick.webp",
+        brick2: "/brick2.webp",
+        plaster: "/plaster.webp",
+        wood: "/wood.webp",
       },
       onChange: (e) => {
-        gsap.to(uniforms.uProgress, { value: e, ease: "linear" });
+        setCurrentWall(nextWall);
+        setNextWall(e);
+        gsap.fromTo(uniforms.uProgress, { value: 0 }, { value: 1, ease: "linear" });
       },
     },
   });
